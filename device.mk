790a2713a7ac867ea732fe1cf391688af8bd82d5
PIPIPIG233666 <pig.priv@gmail.com>
2019-10-01 09:20:13 -0400

    whyred: Build vendor variant of vendor.qti.hardware.camera.device@1.0

--- device.mk
+++ device.mk
@@ -173,7 +173,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Component overrides
 PRODUCT_COPY_FILES += \
