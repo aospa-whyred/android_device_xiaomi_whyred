b92a5e82d443f684c5384bac1af9307b1dfdc1a3
Alastair Donaldson <afdx@google.com>
2020-01-29 15:35:06 +0000

    whyred: Add android.software.vulkan.deqp.level, version 2019-03-01
    This change sets the new Vulkan dEQP level feature flag to the value
    corresponding to the Vulkan dEQP tests for Android 11.
    
    Change-Id: I0715787b310f48c27747849ebd0724e0ca6fbbde

--- device.mk
+++ device.mk
@@ -389,7 +389,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2019-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Power
 $(call inherit-product, vendor/qcom/opensource/power/power-vendor-product.mk)
