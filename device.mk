d7a104d32f2630f3c1df6aa229e9b568bb493496
Edwin Moquete <edwinmmoquete@gmail.com>
2020-12-10 19:55:29 -0400

    whyred: Remove unused camera interfaces

--- device.mk
+++ device.mk
@@ -168,10 +168,8 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
