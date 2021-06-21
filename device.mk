6bcfbfc9bbf428fb09251ad2a4a8cdfc5cc4a395
orges <me@orgesified.ml>
2021-06-21 21:11:24 +0200

    fixup! whyred: build qti bt stack
    Change-Id: Id1cac2e40d5a9c90f3528f85a57be92dfd74796c

--- device.mk
+++ device.mk
@@ -159,6 +159,7 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     Bluetooth \
     libbthost_if \
+    libbluetooth_qti \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
